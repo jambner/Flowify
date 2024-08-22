b0821aef9e495a04c31a27ec3e5a40694f4f450e
jambo <jambosucks@gmail.com>
2024-08-21 19:18:32 -0500

    Initial Commit

---
+++ Flowify/Components.swift
@@ -0,0 +1,32 @@
+//
+//  Components.swift
+//  Flowify
+//
+//  Created by Ramon Martinez on 8/7/24.
+//
+
+import Foundation
+import UIKit
+
+
+class Components: UIViewController {
+    
+    lazy var textBar: UITextField = {
+        textBar = UITextField()
+        textBar.placeholder = "Type here..."
+        textBar.borderStyle = .roundedRect
+        textBar.backgroundColor = .gray
+        return textBar
+    }()
+    
+    lazy var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Play", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+}
---
+++ Flowify/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  Flowify
+//
+//  Created by Ramon Martinez on 7/13/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        let mainViewController = MainViewController()
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        window.rootViewController = navigationController // Setting root view controller here
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
